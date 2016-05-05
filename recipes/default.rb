4d6589abc319b6698cf85196a5ecf36cfa23d511
Dmitri Meshin <0x7ffec@gmail.com>
2016-05-05 20:59:05 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+name "php"
+description "Install php from source"
+override_attributes(
+  "php" => {
+    "install_method" => "source"
+  }
+)
+run_list(
+  "recipe[php]"
+)
