dffa6a42b107991024606720e2fcfc7f7c7a3c8a
Dmitri Meshin <0x7ffec@gmail.com>
2016-05-06 11:15:12 +0300

    Update install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -6,20 +6,3 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
-package 'php' do
-  action :install
-end
-
-
-package 'apache2' do
-  action :install
-end
-
-package 'apache2::mod_ssl' do
-  action :install
-end
-
-package 'apache2::mod_php5' do
-  action :install
-end
