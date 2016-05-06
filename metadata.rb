ab771ed7a451a6e0a88cd515cf308923ab196a9d
Dmitri Meshin <0x7ffec@gmail.com>
2016-05-06 12:17:12 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,13 @@ description      'Installs/Configures magento'
 long_description 'Installs/Configures magento'
 version          '0.1.0'
 
-depends 'php'
-depends 'apache2'
+depends "resolver"
+depends "build-essential"
+depends "apt"
+depends "git"
+depends "apache2"
+depends "ruby"
+depends "composer"
+depends "php"
+
+supports "ubuntu"
